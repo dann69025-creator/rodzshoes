883d1f075020e8dcf9b1e9c94fa76c6857d4617d
dann69025-creator <dann69025@gmail.com>
2026-07-14 11:56:05 -0500

    v3.0

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // REEMPLAZA 'rodzshoes' por el nombre exacto de tu repositorio en GitHub
-  base: '/rodzshoes/', 
-})
